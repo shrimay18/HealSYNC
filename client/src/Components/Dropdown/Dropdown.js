ceb7041051d70b56ce128eef18863cacb07c5906
shrimay18 <shrimaysomani18@gmail.com>
2024-07-09 00:09:21 +0530

    signup page in progress

---
+++ client/src/Components/Dropdown/Dropdown.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Dropdown.css';
+
+const Dropdown = ({ options, selected, setSelected }) => {
+    return (
+        <div className="dropdown">
+            <select
+                value={selected}
+                onChange={(e) => setSelected(e.target.value)}
+            >
+                {options.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+}
+
+export default Dropdown;
