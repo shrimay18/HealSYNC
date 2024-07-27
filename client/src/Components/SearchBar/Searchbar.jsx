642d8b90080e4b3ecd5dcec41f082e0ad8970d4d
hemkesh <hemkesh123kantawala@gmail.com>
2024-07-27 23:09:32 +0530

    Hospital Dashboard all the functionalities completed

--- client/src/Components/SearchBar/Searchbar.jsx
+++ client/src/Components/SearchBar/Searchbar.jsx
@@ -4,28 +4,25 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './Searchbar.css';
 
 const SearchBar = ({ onSearch }) => {
-  const [query, setQuery] = useState('');
+    const [query, setQuery] = useState('');
 
-  const handleInputChange = (event) => {
-    setQuery(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+        onSearch(event.target.value); // Trigger search on input change
+    };
 
-  const handleSearch = () => {
-    onSearch(query);
-  };
-
-  return (
-    <div className="search-bar">
-      <FontAwesomeIcon icon={faSearch} className="search-icon" />
-      <input
-        type="text"
-        value={query}
-        onChange={handleInputChange}
-        placeholder="Search hospital"
-        className="search-input"
-      />
-    </div>
-  );
+    return (
+        <div className="search-bar">
+            <FontAwesomeIcon icon={faSearch} className="search-icon" />
+            <input
+                type="text"
+                value={query}
+                onChange={handleInputChange}
+                placeholder="Search hospital"
+                className="search-input"
+            />
+        </div>
+    );
 };
 
 export default SearchBar;
