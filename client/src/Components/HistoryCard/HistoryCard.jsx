cead72f2aaf31eab296b522436675a3213d5eaee
shrimay18 <shrimaysomani18@gmail.com>
2024-08-16 10:31:11 +0530

    Appointment page improved

--- client/src/Components/HistoryCard/HistoryCard.jsx
+++ client/src/Components/HistoryCard/HistoryCard.jsx
@@ -12,10 +12,10 @@ function HistoryCard({ history, name }) {
   return (
     <div className="historycard">
       <div className="card-body">
-        <h5 className="card-date"><span className="title">Date: </span>{history.date}</h5>
-        <h5 className="card-complaint"><span className="title">Chief Complaint: </span>{history.chiefComplaint}</h5>
-        <h5 className="card-diagnosis"><span className="title">Diagnosis: </span>{history.diagnosis}</h5>
-        <h5 className="card-doctor"><span className="title">Doctor: </span>{name}</h5>
+        <h2 className="card-heading card-date"><span className="title">Date: </span>{history.date}</h2>
+        <h2 className="card-heading card-complaint"><span className="title">Chief Complaint: </span>{history.chiefComplaint}</h2>
+        <h2 className="card-heading card-diagnosis"><span className="title">Diagnosis: </span>{history.diagnosis}</h2>
+        <h2 className="card-heading card-doctor"><span className="title">Doctor: </span>{name}</h2>
       </div>  
       <div className="card-footer">
         <button className="card-button" onClick={handleEdit}>Edit</button>
